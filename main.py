fa7a06ed226f17c04533a63f0f3103fe200254ec
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-08-20 16:02:35 +0500

    Integrate pydantic model for validating request body.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+
 @app.get("/")
-async def root():
+def root():
     return {"message":"Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"These are your posts"}
+
+@app.post("/createpost")
+def create_post(post: Post):
+    print(post)
+    return {"data": dict(post)}
