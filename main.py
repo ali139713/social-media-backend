035de6926d1e75df506a9326fdaca264b6a5360d
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-08-20 22:44:49 +0500

    Complete API for updating post.

--- main.py
+++ main.py
@@ -31,12 +31,6 @@ def root():
 def get_posts():
     return {"data":my_posts}
 
-@app.post("/createpost", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    post_dict = dict(post)
-    post_dict["id"] = randrange(0, 100000)
-    my_posts.append(post_dict)    
-    return {"data": post_dict}
 
 @app.get("/post/{id}")
 def get_post(id: int):
@@ -45,11 +39,31 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
     return {"data": post}
 
+
+@app.post("/createpost", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = dict(post)
+    post_dict["id"] = randrange(0, 100000)
+    my_posts.append(post_dict)    
+    return {"data": post_dict}
+
+@app.put("/post/{id}")
+def update_post(id:int, post:Post):
+    postIndex = find_index_of_post(id)
+    if postIndex == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} does not exist")
+    print(postIndex)
+    post_dict = dict(post)
+    post_dict["id"] = id
+    my_posts[postIndex] = post_dict
+    return {"data": post_dict}
+
+
 @app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
     postIndex = find_index_of_post(id)
-    if not postIndex:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} was not found")
+    if postIndex == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} does not exist")
     print(postIndex)
     my_posts.pop(postIndex)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
