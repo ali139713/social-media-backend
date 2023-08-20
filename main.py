4a6340a9d699853ad05910ed3ad3d5fb80cb6a20
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-08-20 16:18:01 +0500

    Update and return all in memory posts.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randrange
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
@@ -6,6 +8,10 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"id": 1, "title": "title of post 1", "content": "content of post 1"}, {"id": 2, "title": "My Favourite Food", "content": "pizza"}]
 
 @app.get("/")
 def root():
@@ -13,9 +19,11 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data":"These are your posts"}
+    return {"data":my_posts}
 
 @app.post("/createpost")
 def create_post(post: Post):
-    print(post)
-    return {"data": dict(post)}
+    post_dict = dict(post)
+    post_dict["id"] = randrange(0, 100000)
+    my_posts.append(post_dict)    
+    return {"data": post_dict}
