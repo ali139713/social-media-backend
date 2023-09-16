f810769f734c6ddb334deaef3b22cdf37df69988
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-09-16 14:18:17 +0500

    Remove rating from Post model.

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
 
 my_posts = [{"id": 1, "title": "title of post 1", "content": "content of post 1"}, {"id": 2, "title": "My Favourite Food", "content": "pizza"}]
 
