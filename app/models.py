50ce38915955ddee57ea260b80ed2c7c529076c2
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-09-17 21:34:37 +0500

    Fix default value for timestamp.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 class Post(Base):
@@ -10,4 +11,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='True', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now'))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
