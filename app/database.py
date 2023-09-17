412d8283f1390ae064b3f4d64856a1bb90a35ff2
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-09-17 21:24:52 +0500

    Fix issue error in db connection.

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:aLi721A#dh@localhost/social_med
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
